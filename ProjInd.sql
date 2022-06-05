4a7fe5a409a757a43ca0f331c5d8efa83deac8be
João Gabriel <99890878+joaogabrielmf@users.noreply.github.com>
2022-06-05 14:00:56 -0300

    Add files via upload

---
+++ ProjInd.sql
@@ -0,0 +1,34 @@
+create database legaltec;
+
+use legaltec;
+
+create table clientes (idClientes int primary key auto_increment,
+nomeCliente varchar (45),
+budget varchar (45),
+contato varchar (45));
+
+create table processos (idProcessos int primary key auto_increment,
+numProcesso varchar (45),
+valorCausa varchar (45),
+dataInicial date,
+fk_clientes int,
+foreign key (fk_clientes) references clientes (idClientes),
+sentença date,
+fk_processos int,
+foreign key (fk_processos) references processos (idProcessos));
+
+select numProcesso, valorCausa, dataInicial from processos;
+
+insert into clientes values (null, 'SPTech', '10000', 'digital@sptech.school'),
+(null, 'Anhembi Morumbi', '5000', 'anhembi@anhembimorumbi.com'),
+(null, 'Mackenzie', '100000', 'contato@mackenzie.br');
+
+insert into processos values (null, 'SP0102030405', '10000', '2000-01-19', '1', null, null),
+(null, 'RJ0203040506', '2000', '1996-03-18', '2', null, null),
+(null, 'RS0405060708', '10000', '1998-04-06', '1', null, '2');
+
+select * from processos;
+select * from clientes;
+
+alter table clientes add column email varchar(45);
+alter table clientes add column senha varchar(45);
