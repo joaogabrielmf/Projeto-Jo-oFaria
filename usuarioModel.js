c4aa7de25daba65f34897055e8dd675218bb26d8
João Gabriel <99890878+joaogabrielmf@users.noreply.github.com>
2022-06-08 10:39:11 -0300

    Add files via upload

--- usuarioModel.js
+++ usuarioModel.js
@@ -34,7 +34,7 @@ function cadastrar(nome, email, senha) {
 function bancodedados() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = 
-    `select numProcesso, valorCausa, DATE_FORMAT(dataInicial,'%Y-%m-%d') as dataInicial from processos`;
+    `select honorários, valorCausa from processos`;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
